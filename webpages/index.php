860e539b90b340963a8f65a5914c01eb98afa928
Reece Tarrant <rtarrant70998@gmail.com>
2021-02-07 00:04:49 +0000

    Added data to folder
    Added .CSV and .JSON  data to webpages folder to test the data display and original dataset aspects of the website

--- webpages/index.php
+++ webpages/index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
     <body>
-
+    <li><a href ="data.php"> View Data </a></li>
+    <li><a href = "consumption_of_alcohol_4_or_more_times_a_week.csv"> Original dataset </a></li>
     </body>
 </html>
